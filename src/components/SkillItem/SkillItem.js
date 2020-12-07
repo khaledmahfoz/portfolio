097549d9e20bb922b5c2b1b8e45c1f1071a3bd6c
khaled <khaledma7foz@yahoo.com>
2020-12-08 01:11:55 +0200

    feature added v1

--- src/components/SkillItem/SkillItem.js
+++ src/components/SkillItem/SkillItem.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import AOS from 'aos';
 
 import classes from '../SkillTree/SkillTree.module.scss';
+import "aos/dist/aos.css";
 
 const SkillItem = (props) => {
+   useEffect(() => {
+      AOS.init();
+      AOS.refresh();
+   }, []);
    return (
       <div className={classes.cd_timeline_block}>
          <div className={classes.cd_timeline_img}></div>
-         <div className={classes.cd_timeline_content}>
-            {
-               props.skill.images.map((img, i) => <img className={classes.cd_timeline_content_img} key={i} src={img} alt="skill" />)
-            }
-            <h2>{props.skill.title}</h2>
-            {
-               props.skill.details.map((info, i) => <p key={i}>- {info}</p>)
-            }
-            {/* <div className={classes.timeline_content_info}></div> */}
+         <div data-aos="fade-up" data-aos-duration="300">
+            <div className={classes.cd_timeline_content}>
+               {
+                  props.skill.images.map((img, i) => <img className={classes.cd_timeline_content_img} key={i} src={img} alt="skill" />)
+               }
+               <h2>{props.skill.title}</h2>
+               {
+                  props.skill.details.map((info, i) => <p key={i}>- {info}</p>)
+               }
+               {/* <div className={classes.timeline_content_info}></div> */}
+            </div>
          </div>
       </div>
    )
--- src/components/SkillTree/SkillTree.js
+++ src/components/SkillTree/SkillTree.js
@@ -7,7 +7,11 @@ import SkillItem from '../SkillItem/SkillItem';
 const SkillTree = props => {
    return (
       <section id={classes.cd_timeline} className={classes.cd_container}>
-         {props.data.map((skill, i) => <SkillItem key={i} skill={skill} />)}
+         {props.data.map((skill, i) => {
+            return (
+               <SkillItem key={i} skill={skill} />
+            );
+         })}
       </section>
    )
 }
