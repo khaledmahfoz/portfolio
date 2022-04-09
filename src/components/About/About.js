d90150d2f009ddbdaae2b1bd82dc0b08cf250873
khaled <khaledma7foz@yahoo.com>
2022-04-09 06:21:28 +0200

    replace author img

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,42 +1,51 @@
-import React from 'react';
+import React from "react";
 
-import skillsData from '../../utils/skills.js';
+import skillsData from "../../utils/skills.js";
 
-import authorMD from '../../images/author/author_md.jpg';
+import authorMD from "../../images/author/author.jpg";
 
-import classes from './About.module.scss';
+import classes from "./About.module.scss";
 
-import Container from '../UI/Container/Container';
-import Section from '../Section/Section';
-import SkillTree from '../SkillTree/SkillTree';
+import Container from "../UI/Container/Container";
+import Section from "../Section/Section";
+import SkillTree from "../SkillTree/SkillTree";
 
 const About = () => {
-   return (
-      <div>
-         <Section>
-            <h3>About Me</h3>
-            <p>more about me and my skills</p>
-         </Section>
-         <Container>
-            <div className={classes.About}>
-               <div className={classes.First}>
-                  <div className={classes.Img}>
-                     <img src={authorMD} alt="author" />
-                  </div>
-                  <div className={classes.Info}>
-                     <p>My name is <i><u>khaled mahfoz</u></i>, from egypt and i'm a mern stack developer. i've been in the web development industry since 2016 fighting back to back with <span>js</span> alot of experience was gained and alot yet to come.</p>
-                     <q>I hope that my code will be a good addition to the world</q>
-                  </div>
-               </div>
-               <div className={classes.Final}>
-                  <div className={classes.Skills}>
-                     <SkillTree data={skillsData} />
-                  </div>
-               </div>
-            </div>
-         </Container>
-      </div>
-   );
-}
+	return (
+		<div>
+			<Section>
+				<h3>About Me</h3>
+				<p>more about me and my skills</p>
+			</Section>
+			<Container>
+				<div className={classes.About}>
+					<div className={classes.First}>
+						<div className={classes.Img}>
+							<img src={authorMD} alt="author" />
+						</div>
+						<div className={classes.Info}>
+							<p>
+								My name is{" "}
+								<i>
+									<u>khaled mahfoz</u>
+								</i>
+								, from egypt and i'm a mern stack developer. i've been in the
+								web development industry since 2016 fighting back to back with{" "}
+								<span>js</span> alot of experience was gained and alot yet to
+								come.
+							</p>
+							<q>I hope that my code will be a good addition to the world</q>
+						</div>
+					</div>
+					<div className={classes.Final}>
+						<div className={classes.Skills}>
+							<SkillTree data={skillsData} />
+						</div>
+					</div>
+				</div>
+			</Container>
+		</div>
+	);
+};
 
 export default About;
